3c72ec2f7e859c2f602a71b938b6b1871ba34d52
Artem <art.korka@gmail.com>
2018-05-30 11:45:39 +0300

    Task 3

--- tasks.php
+++ tasks.php
@@ -24,3 +24,28 @@ function reformat($string){
 reformat("liMeSHArp DeveLoper TEST");
 
 
+function next_binary_number($numArray){
+	$array_len = count($numArray);
+	for($i = $array_len-1; $i>=0; $i--)
+	{
+		if($numArray[$i] == 0)
+		{
+			for($j = $i; $j<$array_len; $j++)
+			{
+				$numArray[$j]=0;
+			}
+			$numArray[$i]=1;
+			return $numArray;
+		}
+	}
+	$result = [1];
+	echo '1,';
+	for($i=0; $i<$array_len; $i++)
+	{
+		$result[]=0;
+	}
+	return $result;
+}
+
+var_dump(next_binary_number([1,0,1,0,0,0,1]));
+
